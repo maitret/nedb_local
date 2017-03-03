f4a92a905a51bece58a641e75edad1b9557eaf5f
maitret <maitret@myhostmx.com>
2017-03-03 04:00:48 -0600

    .

--- www/testPersistence2.js
+++ www/testPersistence2.js
@@ -13,48 +13,68 @@ document.getElementById("results").innerHTML = "TESTS FAILED";
 var filename = 'prueba';
 var db = new Nedb({ filename: filename, autoload: true });
 
-function Update_Data(id, column, value){ 
-var new_data = { column: value };
-db.update({ _id: id },{ $set: { new_data }}, { multi: true }, function (err, numReplaced) {
-console.log(id+": "+column+": "+value+", numReplaced: "+numReplaced); 
+function Update_Data(id, value){ 
+db.update({ _id: id },{ $set: { sync: value }}, { multi: true }, function (err, numReplaced) {
+console.log(id+" / sync: "+value+", numReplaced: "+numReplaced); 
 });
 }
 
 //var data = [];
 var date = ""+Date.now()+"";
 var data = {
-name: 'Luigui',
-twitter: '@maitret', 
-date: date
+nombre: 'Luigui',
+fecha: date,
+sync: 'Pendiente'
 }; 
 
-db.insert(data, function(err, doc) { console.log('Inserted: '+doc.name+' with ID '+ doc._id); }); 
+db.insert(data, function(err, doc) { console.log('Inserted: '+doc.nombre+' with ID '+ doc._id); }); 
 
-db.find({ date: /10/ }, function (err, docs){
+/* db.find({ date: /10/ }, function (err, docs){
 if (docs.length >= 1){ 
 console.log("docs.length: "+docs.length); 
 docs.forEach(function(d){
-Update_Data(d._id, "name", 'ÑOla k ase!ó'); 
 $(".res_sults").append(JSON.stringify(d)+"<br>"); 
 });
 }
-}); 
+}); */ 
 
-db.find({}, function (err, docs){
+//db.find({}, function (err, docs){
+db.find({  }).sort({ fecha: 1 }).exec(function (err, docs) {
 console.log("docs.length 2: "+docs.length);
 docs.forEach(function(d){
-$(".res_sults_2").append(JSON.stringify(d)+"<br>"); 
+var table_db = '<tr>'+
+'<td>'+d.nombre+'</td>'+
+'<td>'+d.fecha+'</td>'+
+'<td class="sync_'+d._id+'">'+d.sync+'</td>'+
+'</tr>';
+$("#table_db").append(table_db); 
+//$(".res_sults_2").append(JSON.stringify(d)+"<br>"); 
+});
+});
+
+function Update_All(){
+db.find({}, function (err, docs){
+docs.forEach(function(d){
+if(d.sync != "OK"){
 $.ajax({
 method: "POST",
 url: "https://www.enatural.tk/apps/test/get_data.php",
 data: d
-})
-.done(function( msg ) { 
-//alert(msg);
+}).done(function(msg){ 
+Update_Data(d._id, "OK"); 
+$(".sync_"+d._id).html("OK");
 });
+}
+});
+});
+}
 
+function Delete_All(){
+db.remove({}, { multi: true }, function (err, numRemoved) { 
+alert("Borrados: "+numRemoved); 
+location = "index.html?deleted="+numRemoved;
 });
-});
+}
 
 /* if (docs.length !== 1) {
 //console.log(docs);
